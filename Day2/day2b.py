73a38beef16e42ae5c267815a3987b4ada1d6d50
Ruben Marin Grez <rumaringr@gmail.com>
2023-12-02 15:33:19 +0100

    Day 2
    fertig

--- Day2/day2b.py
+++ Day2/day2b.py
@@ -1,6 +1,6 @@
 import re
 
-file = open("test_data_b.txt")
+file = open("input.txt")
 
 content = file.readlines()
 
@@ -16,21 +16,23 @@ minNumBlue = [0] * (len(content))
 minNumGreen = [0] * (len(content))
 
 for i in range(len(content)): 
-    
-    setRed = [0] * 3
-    setBlue = [0] * 3 
-    setGreen = [0] * 3   
+      
     cnt = 0
     
     numSet = content[i].split(";")
 
+    setRed = [0] * len(numSet)
+    setBlue = [0] * len(numSet)
+    setGreen = [0] * len(numSet) 
+
     for s in numSet: 
-    #print(s)
+        #print(s)
         
         match = re.search(r'\d+ blue',s)
         if match:
    #         print(match[0])
             tmp = re.search(r'\d+',match[0])
+            #print(cnt)
             setBlue[cnt] = int(tmp[0])
             
     
@@ -61,9 +63,9 @@ for i in range(len(content)):
 #    print("b: ")
 #    print(setBlue)
 
-    minNumRed[i] = setRed[2]
-    minNumGreen[i] = setGreen[2]
-    minNumBlue[i] = setBlue[2]
+    minNumRed[i] = setRed[len(numSet)-1]
+    minNumGreen[i] = setGreen[len(numSet)-1]
+    minNumBlue[i] = setBlue[len(numSet)-1]
 
 
 for i in range(len(content)):
