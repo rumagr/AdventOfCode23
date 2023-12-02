061c6e401a73ebc0a09489e8831fe705ffcd9db7
Ruben Marin Grez <rumaringr@gmail.com>
2023-12-02 02:52:51 +0100

    Day 1
    b ?

--- Day1/day1.py
+++ Day1/day1.py
@@ -4,6 +4,40 @@ file = open("input.txt")
 
 content = file.readlines()
 
+dSpelled = ["one","two","three","four","five","six","seven","eight","nine"]
+digit = ["1","2","3","4","5","6","7","8","9"]
+
+dMap = dict(zip(dSpelled,digit))
+
+foo = 0; 
+
+print(dMap)
+
+for i in range(len(content)):
+    for a in range(len(content[i])):
+        for y in dSpelled:
+            if y in content[i][0:a]:
+                content[i] = content[i].replace(y,dMap[y]) 
+                foo = 1                
+                break
+        if foo > 0: 
+            foo = 0
+            break
+
+for i in range(len(content)):
+    for a in range(len(content[i])):
+        b = len(content[i])-a        
+        for y in dSpelled:
+            if y in content[i][b:len(content[i])]:
+                content[i] = content[i].replace(y,dMap[y])                
+                foo = 1                
+                break
+        if foo > 0: 
+            foo = 0 
+            break            
+
+
+
 num = [] 
 
 res = 0
@@ -15,7 +49,7 @@ for x in content:
     if re.search(p, x) is not None: 
         for catch in re.finditer(p, x):      
             num.append(catch[0])
-    print(num)
+    #print(num)
     if len(num) > 1: 
         temp = int(num[0])*10 + int(num[len(num)-1]) 
         res += temp
